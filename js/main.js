6a9660ca23b696a1213d2ee697dec589e115a66c
Matheus Barone <matheusvbarone@gmail.com>
2017-07-30 20:32:57 -0300

    Criando uma função que irá  gerar html na página quando clicar o botão

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-//SETANDO A VARIAVEL DE REQUISIÇÃO
-var myRequest = new XMLHttpRequest();
-//PEGANDO AS INFORMAÇÕES DO JSON
-myRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-//CARREGANDO AS INFORMAÇÕES DO JSON
-myRequest.onload = function(){
-    var myData = JSON.parse(myRequest.responseText);
-    console.log(myData[0]);
-};
-//ENVIANDO AS INFORMAÇÕES DO JSON
-myRequest.send();
+//CRIANDO A VARIAVEL PARA A DIV COM O CONTEUDO QUE SERÁ CARREGADO
+var animalContainer = document.getElementById("animal-info");
+//CRIANDO A VARIAVEL DO BOTÃO
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    //SETANDO A VARIAVEL DE REQUISIÇÃO
+    var myRequest = new XMLHttpRequest();
+    //PEGANDO AS INFORMAÇÕES DO JSON
+    myRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    //CARREGANDO AS INFORMAÇÕES DO JSON
+    myRequest.onload = function(){
+        var myData = JSON.parse(myRequest.responseText);
+        renderHTML(myData);
+    };
+    //ENVIANDO AS INFORMAÇÕES DO JSON
+    myRequest.send();
+});
+
+//RENDERIAR O HTML NA PÁGINA
+
+function renderHTML(data){
+    var htmlString = ""
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
+
